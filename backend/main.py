81e9a5361fe9aeff619dcd27a27c9688a906d502
Varun-310 <itsvarun310@gmail.com>
2026-03-04 11:33:31 +0530

    Render connection

--- backend/main.py
+++ backend/main.py
@@ -89,9 +89,10 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "main:app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=port,
         reload=settings.DEBUG
     )
